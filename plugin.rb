c4d142d0bc87f454eb94b840c589c3b034d6eaf5
mimi-katsu <maiamor1000@gmail.com>
2026-02-16 17:20:42 -0800

    a

--- plugin.rb
+++ plugin.rb
@@ -3,9 +3,6 @@
 # name: terminal_page
 # version: 0.1
 
-register_asset "javascripts/discourse/terminal-route-map.js"
-register_asset "javascripts/discourse/routes/terminal.js"
-
 after_initialize do
   # JSON endpoint (data)
   Discourse::Application.routes.append do
